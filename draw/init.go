13dbf25a53131f27a07a71ad808c1ee45ff462e2
hellflame <hellflamedly@gmail.com>
2019-12-03 22:42:00 +0800

    get terminal size

--- draw/init.go
+++ draw/init.go
@@ -1,4 +1,31 @@
 package draw
 
+import (
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
 const VERSION = "0.1.0"
 
+var DefaultWidth = 50
+var DefaultHeight = 80
+var IsWindows = false
+
+func init() {
+	if runtime.GOOS == "windows" {
+		IsWindows = true
+	} else {
+		cmd := exec.Command("stty", "size")
+		cmd.Stdin = os.Stdin // this is important
+		result, err := cmd.Output()
+		if err == nil {
+			parse := strings.Split(strings.TrimRight(string(result), "\n"),
+				" ")
+			DefaultHeight, _ = strconv.Atoi(parse[0])
+			DefaultWidth, _ = strconv.Atoi(parse[1])
+		}
+	}
+}
